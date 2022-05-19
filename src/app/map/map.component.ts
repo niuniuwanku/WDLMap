9db7211cc02501211caa6e31f4cfa6e1c4cc5fae
niuniuwanku <niuniu970624@gmail.com>
2022-05-19 17:58:57 -0400

    -map done without weight

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
 import {HttpClient} from "@angular/common/http";
 import {Map} from "./map";
+import {map} from "rxjs";
 
 
 
@@ -11,37 +12,43 @@ import {Map} from "./map";
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  Map: Map[] = [];
+  latnew: number = 0;
+  longnew: number = 0;
+  public coords: google.maps.LatLng[] = []
 
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getConfig().subscribe((data:Map[]) => {this.Map = data},
+      error => console.log(error),
+      () => {
+        for(let latlong of this.Map){
+          this.coords.push(new google.maps.LatLng(Number(latlong.lat),Number(latlong.long)))
+        }
+        console.log(this.coords)
+      }
+    )
   }
 
-  lat = 37.775
-  long = -122.434
+  lat = 9.934739
+  long = -84.087502
 
   getConfig() {
-    return this.http.get<Map>("http://127.0.0.1:5000/heatmap");
+    return this.http.get<Map[]>("http://127.0.0.1:5000/heatmap");
   }
+
+
   // @ts-ignore
   private map: google.maps.Map = null;
   // @ts-ignore
   private heatmap: google.maps.visualization.HeatmapLayer = null;
   onMapLoad(mapInstance: google.maps.Map) {
     this.map = mapInstance;
-
-    // here our in other method after you get the coords; but make sure map is loaded
-
-    // const coords: google.maps.LatLng[] = []
-    const coords: google.maps.LatLng[] = [new google.maps.LatLng(37.782551, -122.445368),]
-    //   new google.maps.LatLng(37.782745, -122.444586),
-    //   new google.maps.LatLng(37.782842, -122.443688),
-
     this.heatmap = new google.maps.visualization.HeatmapLayer({
       map: this.map,
-      data: coords
+      data: this.coords
     });
   }
 }
--- src/app/map/map.ts
+++ src/app/map/map.ts
@@ -1,5 +1,14 @@
-export interface Map {
-  lat:number
-  long:number
-  Count:number
+// export interface Map {
+//   lat:number
+//   long:number
+//   Count:number
+// }
+
+export class Map {
+  constructor(
+    public lat: number,
+    public long: number,
+    public Count: number
+) {
+}
 }
